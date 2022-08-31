a833a5ffdfdafabbbb332d9d6c4a79c2ad4a671a
Steve Wortmann <steven.wortmann@gmail.com>
2022-08-31 14:40:59 -0400

    Created insertWorkVisits stored proc

--- init_db_stored_procs.sql
+++ init_db_stored_procs.sql
@@ -164,8 +164,8 @@ END;
 CREATE PROCEDURE insertHomeVisits(
   a_placekey VARCHAR(max),
   w_daterangestart VARCHAR(max),
-  af_visitorhomecbg VARCHAR(max),
-  af_visitorhomecbg_cnt INT,
+  ad_visitorhomecbg VARCHAR(max),
+  ad_visitorhomecbg_cnt INT,
 )
 AS $$
 DECLARE @vidout INT;
@@ -176,8 +176,29 @@ BEGIN
   IF (SELECT COUNT(1) FROM visitsInfo WHERE (placekey=a_placekey AND week_begin=w_daterangestart))=1 
     BEGIN
         SELECT vid INTO @vidout FROM visitsInfo WHERE (placekey=a_placekey AND week_begin=w_daterangestart)
-        SELECT cbgid INTO @cbgidout FROM censusBlockGroups WHERE (cbg_number=af_visitorhomecbg)
+        SELECT cbgid INTO @cbgidout FROM censusBlockGroups WHERE (cbg_number=ad_visitorhomecbg)
         INSERT INTO homeVisits(vid, cbgid, visit_count)
-        VALUES (@vidout, @cbgidout, af_visitorhomecbg_cnt);
+        VALUES (@vidout, @cbgidout, ad_visitorhomecbg_cnt);
+    END IF;
+END;
+
+CREATE PROCEDURE insertWorkVisits(
+  a_placekey VARCHAR(max),
+  w_daterangestart VARCHAR(max),
+  af_visitordaytimecbg VARCHAR(max),
+  af_visitordaytimecbg_cnt INT,
+)
+AS $$
+DECLARE @vidout INT;
+DECLARE @cbgidout INT;
+
+BEGIN
+
+  IF (SELECT COUNT(1) FROM visitsInfo WHERE (placekey=a_placekey AND week_begin=w_daterangestart))=1 
+    BEGIN
+        SELECT vid INTO @vidout FROM visitsInfo WHERE (placekey=a_placekey AND week_begin=w_daterangestart)
+        SELECT cbgid INTO @cbgidout FROM censusBlockGroups WHERE (cbg_number=af_visitordaytimecbg)
+        INSERT INTO homeVisits(vid, cbgid, visit_count)
+        VALUES (@vidout, @cbgidout, af_visitordaytimecbg_cnt);
     END IF;
 END;
--- insertWorkVisits_StoredProc.sql
+++ insertWorkVisits_StoredProc.sql
@@ -1,8 +1,8 @@
-CREATE PROCEDURE insertHomeVisits(
+CREATE PROCEDURE insertWorkVisits(
   a_placekey VARCHAR(max),
   w_daterangestart VARCHAR(max),
-  ad_visitorhomecbg VARCHAR(max),
-  ad_visitorhomecbg_cnt INT,
+  af_visitordaytimecbg VARCHAR(max),
+  af_visitordaytimecbg_cnt INT,
 )
 AS $$
 DECLARE @vidout INT;
@@ -13,8 +13,8 @@ BEGIN
   IF (SELECT COUNT(1) FROM visitsInfo WHERE (placekey=a_placekey AND week_begin=w_daterangestart))=1 
     BEGIN
         SELECT vid INTO @vidout FROM visitsInfo WHERE (placekey=a_placekey AND week_begin=w_daterangestart)
-        SELECT cbgid INTO @cbgidout FROM censusBlockGroups WHERE (cbg_number=ad_visitorhomecbg)
+        SELECT cbgid INTO @cbgidout FROM censusBlockGroups WHERE (cbg_number=af_visitordaytimecbg)
         INSERT INTO homeVisits(vid, cbgid, visit_count)
-        VALUES (@vidout, @cbgidout, ad_visitorhomecbg_cnt);
+        VALUES (@vidout, @cbgidout, af_visitordaytimecbg_cnt);
     END IF;
 END;
