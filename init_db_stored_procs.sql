c8e7387804121b3f6955f82b15517030646d41d9
Steve Wortmann <steven.wortmann@gmail.com>
2022-08-31 14:49:30 -0400

    Created insertBrandsDay stored proc.

--- init_db_stored_procs.sql
+++ init_db_stored_procs.sql
@@ -223,3 +223,24 @@ BEGIN
         VALUES (@vidout, @bidout, al_relatedsameweekbrand_cnt);
     END IF;
 END;
+
+CREATE PROCEDURE insertBrandsDay(
+  a_placekey VARCHAR(max),
+  w_daterangestart VARCHAR(max),
+  ak_relatedsamedaybrand VARCHAR(max),
+  ak_relatedsamedaybrand_cnt INT,
+)
+AS $$
+DECLARE @vidout INT;
+DECLARE @bidout INT;
+
+BEGIN
+
+  IF (SELECT COUNT(1) FROM visitsInfo WHERE (placekey=a_placekey AND week_begin=w_daterangestart))=1 
+    BEGIN
+        SELECT vid INTO @vidout FROM visitsInfo WHERE (placekey=a_placekey AND week_begin=w_daterangestart)
+        SELECT bid INTO @bidout FROM brandsInfo WHERE (brand_name=ak_relatedsamedaybrand)
+        INSERT INTO brandsDay(vid, bid, visit_count)
+        VALUES (@vidout, @bidout, ak_relatedsamedaybrand_cnt);
+    END IF;
+END;
