a569e3da9052aee105c9a58612f287458eae99d2
Steve Wortmann <steven.wortmann@gmail.com>
2023-03-01 10:18:45 -0500

    create postgresql command for 'deviceLog' table population

---
+++ insertDeviceCount_psql.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE PROCEDURE insertDeviceCount( -- 'device_name' field fully populated with init_db_tables
+	 a_placekey VARCHAR,
+	 w_daterangestart VARCHAR,
+	 am_devicetype VARCHAR,
+	 am_devicetype_cnt INT
+)
+AS 
+$$
+DECLARE didout INT;
+DECLARE vidout INT;
+
+BEGIN
+
+  IF (SELECT COUNT(*) FROM visitsInfo v JOIN locationInfo l ON v.locid=l.locid WHERE (l.placekey=a_placekey AND v.week_begin=w_daterangestart))=1 
+    THEN
+        SELECT vid INTO vidout FROM visitsInfo v JOIN locationInfo l ON v.locid=l.locid WHERE (l.placekey=a_placekey AND v.week_begin=w_daterangestart);
+        SELECT did INTO didout FROM devices WHERE (device_name=am_devicetype);
+        INSERT INTO deviceLog(did, vid, user_count)
+            VALUES (didout, vidout, am_devicetype_cnt);
+    END IF;
+END;
