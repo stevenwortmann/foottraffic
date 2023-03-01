5c42b545d50e797fe96ceebaeb37fe1d6de44465
Steve Wortmann <steven.wortmann@gmail.com>
2023-03-01 10:52:37 -0500

    create visitsType record insertion for work/daytime cbg records

---
+++ insertWorkVisits_psql.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE PROCEDURE insertHomeVisits(
+  a_placekey VARCHAR,
+  w_daterangestart DATE,
+  ad_visitordaytimecbg VARCHAR,
+  ad_visitordaytimecbg_cnt INT
+)
+AS $$
+DECLARE vidout INT;
+DECLARE cbgidout INT;
+DECLARE locidout INT;
+BEGIN
+  SELECT locid INTO locidout FROM locationInfo WHERE placekey=a_placekey;
+
+  IF EXISTS (SELECT 1 FROM censusBlockGroups WHERE cbg_number=ad_visitordaytimecbg) THEN
+    SELECT cbgid INTO cbgidout FROM censusBlockGroups WHERE cbg_number=ad_visitordaytimecbg;
+  ELSE
+    INSERT INTO censusBlockGroups(cbg_number)
+        VALUES (ad_visitordaytimecbg);
+    SELECT LASTVAL() INTO cbgidout;
+  END IF;
+
+  IF EXISTS (SELECT 1 FROM visitsInfo v JOIN locationInfo l ON v.locid=l.locid WHERE (l.placekey=a_placekey AND v.week_begin=w_daterangestart)) THEN
+    SELECT vid INTO vidout FROM visitsInfo v JOIN locationInfo l ON v.locid=l.locid WHERE (l.placekey=a_placekey AND v.week_begin=w_daterangestart);
+    INSERT INTO visitsType(locid, vid, cbgid, visit_count, home_work_ind)
+        VALUES (locidout, vidout, cbgidout, ad_visitordaytimecbg_cnt, 'w');
+  END IF;
+END;
