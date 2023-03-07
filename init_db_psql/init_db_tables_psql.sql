f087e291fc0ef67d9907783ad0d8284a514bd8ed
Steve Wortmann <steven.wortmann@gmail.com>
2023-03-07 00:10:45 -0500

    Updated to "created if not exists"

--- init_db_psql/init_db_tables_psql.sql
+++ init_db_psql/init_db_tables_psql.sql
@@ -1,27 +1,22 @@
-
-
-DROP TABLE IF EXISTS relatedBrands, categoriesXref, categories, deviceLog, devices,
-	homeVisits, workVisits, visitsInfo, locationInfo, censusBlockGroups, brandsInfo, naicsCodes;
-
-CREATE TABLE naicsCodes (
+CREATE TABLE IF NOT EXISTS naicsCodes (
     nid INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     top_category VARCHAR(100) NOT NULL,
     sub_category VARCHAR(100),
     naics_code VARCHAR(20)
-    );
+);
 
-CREATE TABLE brandsInfo (
-	bid INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	nid INT REFERENCES naicsCodes (nid) ON DELETE CASCADE,
-	brand_name VARCHAR(100)
-	);
+CREATE TABLE IF NOT EXISTS brandsInfo (
+    bid INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    nid INT REFERENCES naicsCodes (nid) ON DELETE CASCADE,
+    brand_name VARCHAR(100)
+);
 
-CREATE TABLE censusBlockGroups (
-	cbgid INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	cbg_number VARCHAR(12) NOT NULL
-	);
+CREATE TABLE IF NOT EXISTS censusBlockGroups (
+    cbgid INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    cbg_number VARCHAR(12) NOT NULL
+);
 
-CREATE TABLE locationInfo (
+CREATE TABLE IF NOT EXISTS locationInfo (
     locid INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nid INT REFERENCES naicsCodes (nid) ON DELETE CASCADE,
     bid INT REFERENCES brandsInfo (bid) ON DELETE CASCADE,
@@ -35,62 +30,62 @@ CREATE TABLE locationInfo (
     region VARCHAR(5) NOT NULL,
     postal_code VARCHAR(5) NOT NULL,
     phone_number VARCHAR(11)
-    );
+);
 
-CREATE TABLE visitsInfo (
-	vid INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	locid INT REFERENCES locationInfo (locid) ON DELETE CASCADE,
-	week_begin DATE NOT NULL,
-	raw_visit_counts INT NOT NULL,
-	raw_visitor_counts INT NOT NULL,
-	distance_from_home INT NOT NULL,
-	median_dwell FLOAT,
-	normalized_visits_by_state_scaling FLOAT NOT NULL,
-	normalized_visits_by_region_naics_visits FLOAT NOT NULL,
-	normalized_visits_by_region_naics_visitors FLOAT NOT NULL,
-	normalized_visits_by_total_visits FLOAT NOT NULL,
-	normalized_visits_by_total_visitors FLOAT NOT NULL
-    );
+CREATE TABLE IF NOT EXISTS visitsInfo (
+    vid INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    locid INT REFERENCES locationInfo (locid) ON DELETE CASCADE,
+    week_begin DATE NOT NULL,
+    raw_visit_counts INT NOT NULL,
+    raw_visitor_counts INT NOT NULL,
+    distance_from_home INT NOT NULL,
+    median_dwell FLOAT,
+    normalized_visits_by_state_scaling FLOAT NOT NULL,
+    normalized_visits_by_region_naics_visits FLOAT NOT NULL,
+    normalized_visits_by_region_naics_visitors FLOAT NOT NULL,
+    normalized_visits_by_total_visits FLOAT NOT NULL,
+    normalized_visits_by_total_visitors FLOAT NOT NULL
+);
 
-CREATE TABLE visitsType (
+CREATE TABLE IF NOT EXISTS visitsType (
     vtid INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	locid INT REFERENCES locationInfo (locid) ON DELETE CASCADE,
-	vid INT REFERENCES visitsInfo (vid) ON DELETE CASCADE,
+    locid INT REFERENCES locationInfo (locid) ON DELETE CASCADE,
+    vid INT REFERENCES visitsInfo (vid) ON DELETE CASCADE,
     cbgid INT REFERENCES censusBlockGroups (cbgid) ON DELETE CASCADE,
-	visit_count INT NOT NULL,
+    visit_count INT NOT NULL,
     home_work_ind CHAR(1)
-    );
+);
 
-CREATE TABLE devices (
+CREATE TABLE IF NOT EXISTS devices (
     did INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     device_type VARCHAR(7) NOT NULL
-    );
+);
 
-CREATE TABLE deviceLog (
-	dlid INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	vid INT REFERENCES visitsInfo (vid) ON DELETE CASCADE,
-	did INT REFERENCES devices (did) ON DELETE CASCADE,
-	user_count INT NOT NULL
-    );
+CREATE TABLE IF NOT EXISTS deviceLog (
+    dlid INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    vid INT REFERENCES visitsInfo (vid) ON DELETE CASCADE,
+    did INT REFERENCES devices (did) ON DELETE CASCADE,
+    user_count INT NOT NULL
+);
 
-CREATE TABLE categories (
-	cid INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	category_name VARCHAR(100) NOT NULL
-    );
+CREATE TABLE IF NOT EXISTS categories (
+    cid INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    category_name VARCHAR(100) NOT NULL
+);
 
-CREATE TABLE categoriesXref (
+CREATE TABLE IF NOT EXISTS categoriesXref (
     cxid INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	locid INT REFERENCES locationInfo (locid) ON DELETE CASCADE,
-	cid INT REFERENCES categoriesInfo (cid) ON DELETE CASCADE
-    );
+    locid INT REFERENCES locationInfo (locid) ON DELETE CASCADE,
+    cid INT REFERENCES categoriesInfo (cid) ON DELETE CASCADE
+);
 
-CREATE TABLE relatedBrands (
+CREATE TABLE IF NOT EXISTS relatedBrands (
     blid INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     bid INT REFERENCES brandsInfo (bid) ON DELETE CASCADE,
-	vid INT REFERENCES visitsInfo (vid) ON DELETE CASCADE,
-	visit_count INT NOT NULL,
+    vid INT REFERENCES visitsInfo (vid) ON DELETE CASCADE,
+    visit_count INT NOT NULL,
     day_week_ind CHAR(1)
-    );
+);
 
-INSERT INTO devices (device_type) VALUES ('android');
-INSERT INTO devices (device_type) VALUES ('ios');
+INSERT INTO devices (device_type) VALUES ('android') ON CONFLICT DO NOTHING;
+INSERT INTO devices (device_type) VALUES ('ios') ON CONFLICT DO NOTHING;
