446a678ef8e495bbe1a7cb29eb4a025d9a8d0b5f
Steve Wortmann <steven.wortmann@gmail.com>
2023-03-08 12:32:05 -0500

    removed foreign keys from .set_index()

--- init_db_pd/init_db_tables_pd.py
+++ init_db_pd/init_db_tables_pd.py
@@ -5,7 +5,7 @@ naicsCodes = pd.DataFrame(columns=['nid', 'top_category', 'sub_category', 'naics
                           dtype={'nid': 'int', 'top_category': 'object', 'sub_category': 'object', 'naics_code': 'object'}).set_index('nid')
 
 brandsInfo = pd.DataFrame(columns=['bid', 'nid', 'brand_name'],
-                          dtype={'bid': 'int', 'nid': 'int', 'brand_name': 'object'}).set_index(['bid','nid'])
+                          dtype={'bid': 'int', 'nid': 'int', 'brand_name': 'object'}).set_index('bid')
 
 censusBlockGroups = pd.DataFrame(columns=['cbgid', 'cbg_number'],
                                  dtype={'cbgid': 'int', 'cbg_number': 'object'}).set_index('cbgid')
@@ -15,7 +15,7 @@ locationInfo = pd.DataFrame(columns=['locid', 'nid', 'bid', 'cbgid', 'placekey',
                             dtype={'locid': 'int', 'nid': 'int', 'bid': 'int', 'cbgid': 'int',
                                    'placekey': 'object', 'location_name': 'object', 'latitude': 'object', 'longitude': 'object',
                                    'street_address': 'object', 'city': 'object', 'region': 'object', 'postal_code': 'object',
-                                   'phone_number': 'object'}).set_index(['locid','nid','bid','cbgid'])
+                                   'phone_number': 'object'}).set_index('locid')
 
 visitsInfo = pd.DataFrame(columns=['vid', 'locid', 'week_begin', 'raw_visit_counts', 'raw_visitor_counts', 'distance_from_home',
                                    'median_dwell', 'normalized_visits_by_state_scaling', 'normalized_visits_by_region_naics_visits',
@@ -25,11 +25,11 @@ visitsInfo = pd.DataFrame(columns=['vid', 'locid', 'week_begin', 'raw_visit_coun
                                  'raw_visitor_counts': 'int', 'distance_from_home': 'int', 'median_dwell': 'float',
                                  'normalized_visits_by_state_scaling': 'float', 'normalized_visits_by_region_naics_visits': 'float',
                                  'normalized_visits_by_region_naics_visitors': 'float', 'normalized_visits_by_total_visits': 'float',
-                                 'normalized_visits_by_total_visitors': 'float'}).set_index(['vid','locid'])
+                                 'normalized_visits_by_total_visitors': 'float'}).set_index('vid')
 
 visitsType = pd.DataFrame(columns=['vtid', 'locid', 'vid', 'cbgid', 'visit_count', 'home_work_ind'],
                           dtype={'vtid': 'int', 'locid': 'int', 'vid': 'int', 'cbgid': 'int',
-                                 'visit_count': 'int', 'home_work_ind': 'object'}).set_index(['vtid','locid','vid','cbgid'])
+                                 'visit_count': 'int', 'home_work_ind': 'object'}).set_index('vtid')
 
 devices = pd.DataFrame({'did': [1, 2],
                         'device_type': ['android', 'ios']},
@@ -38,14 +38,14 @@ devices = pd.DataFrame({'did': [1, 2],
 
 
 deviceLog = pd.DataFrame(columns=['dlid', 'vid', 'did', 'user_count'],
-                          dtype={'dlid': 'int', 'vid': 'int', 'did': 'int', 'user_count': 'int'}).set_index(['dlid','vid','did'])
+                          dtype={'dlid': 'int', 'vid': 'int', 'did': 'int', 'user_count': 'int'}).set_index('dlid')
 
 categories = pd.DataFrame(columns=['cid', 'category_name'],
                           dtype={'cid': 'int', 'category_name': 'object'}).set_index('cid')
 
 categoriesXref = pd.DataFrame(columns=['cxid', 'locid', 'cid'],
-                              dtype={'cxid': 'int', 'locid': 'int', 'cid': 'int'}).set_index(['cxid','locid','cid'])
+                              dtype={'cxid': 'int', 'locid': 'int', 'cid': 'int'}).set_index('cxid')
 
 relatedBrands = pd.DataFrame(columns=['blid', 'bid', 'vid', 'visit_count', 'day_week_ind'],
-                              dtype={'blid': 'int', 'bid': 'int', 'vid': 'int', 'visit_count': 'int', 'day_week_ind': 'object'}).set_index(['blid','bid','vid'])
+                              dtype={'blid': 'int', 'bid': 'int', 'vid': 'int', 'visit_count': 'int', 'day_week_ind': 'object'}).set_index('blid')
 
