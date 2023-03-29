0c29bf6cccff783d8f8f3a926bb4440277f8a21f
Steve Wortmann <steven.wortmann@gmail.com>
2023-03-29 13:48:42 -0400

    debug insertFootTrafficRecord stored proc

--- init_db_tsql/insertFootTrafficRecord_tsql.sql
+++ init_db_tsql/insertFootTrafficRecord_tsql.sql
@@ -1,4 +1,4 @@
-ALTER PROCEDURE [dbo].[insertFootTrafficRecord](
+CREATE OR ALTER PROCEDURE [dbo].[insertFootTrafficRecord](
 	@a_placekey VARCHAR(max),
 	@c_locationname VARCHAR(max),
 	@e_brands VARCHAR(max),
@@ -26,6 +26,9 @@ ALTER PROCEDURE [dbo].[insertFootTrafficRecord](
 )
 AS
 BEGIN
+
+SET NOCOUNT ON
+
 DECLARE @nidout INT;
 DECLARE @bidout INT;
 DECLARE @cbgidout INT;
@@ -41,8 +44,8 @@ DECLARE @vidout INT;
     ELSE
       BEGIN
 		INSERT INTO naicsCodes(naics_code, top_category, sub_category)
-		VALUES (@h_naicscode, @f_topcategory, @g_subcategory);
-		SET @nidout=(SELECT TOP 1 nid FROM naicsCodes ORDER BY nid DESC);
+			VALUES (@h_naicscode, @f_topcategory, @g_subcategory);
+		SET @nidout=SCOPE_IDENTITY();
       END;
 
 	IF (SELECT COUNT(1) FROM brandsInfo WHERE brand_name=@e_brands)=1
@@ -57,7 +60,7 @@ DECLARE @vidout INT;
 		BEGIN
 			INSERT INTO brandsInfo(nid, brand_name)
 				VALUES (@nidout, @e_brands);
-			SET @bidout=(SELECT TOP 1 bid FROM brandsInfo ORDER BY bid DESC);
+			SET @bidout=SCOPE_IDENTITY();
 		END;
 
     IF (SELECT COUNT(1) FROM censusBlockGroups WHERE cbg_number=@ac_poicbg)=1
@@ -69,7 +72,7 @@ DECLARE @vidout INT;
       BEGIN
         INSERT INTO censusBlockGroups(cbg_number)
 			VALUES (@ac_poicbg);
-		SET @cbgidout=(SELECT TOP 1 cbgid FROM censusBlockGroups ORDER BY cbgid DESC);
+		SET @cbgidout=SCOPE_IDENTITY();
       END;
 
     IF (SELECT COUNT(1) FROM locationInfo WHERE placekey=@a_placekey)=1
@@ -80,7 +83,7 @@ DECLARE @vidout INT;
       BEGIN
         INSERT INTO locationInfo(nid, bid, cbgid, placekey, location_name, latitude, longitude, street_address, city, region, postal_code, phone_number)
         	VALUES (@nidout, @bidout, @cbgidout, @a_placekey, @c_locationname, @i_latitude, @j_longitude, @k_streetaddress, @l_city, @m_region, @n_postalcode, @p_phonenumber);
-		SET @locidout=(SELECT TOP 1 locid FROM locationInfo ORDER BY locid DESC);
+		SET @locidout=SCOPE_IDENTITY();
       END;
 
     IF (SELECT COUNT(1) FROM visitsInfo WHERE (locid=@locidout AND week_begin=@w_daterangestart))=1 
@@ -93,10 +96,9 @@ DECLARE @vidout INT;
           normalized_visits_by_region_naics_visits, normalized_visits_by_region_naics_visitors, normalized_visits_by_total_visits, normalized_visits_by_total_visitors)
 			VALUES (@locidout, @w_daterangestart, @y_rawvisitcounts, @z_rawvisitorcounts, @ah_distancefromhome, @ai_mediumdwell, @an_normvisits_statescaling,
 			@ao_normvisits_regionnaicsvisits, @ap_normvisits_regionnaicsvisitors, @aq_normvisits_totalvisits, @ar_normvisits_totalvisitors);
-		SET @vidout=(SELECT TOP 1 vid FROM visitsInfo ORDER BY vid DESC);
+		SET @vidout=SCOPE_IDENTITY();
       END;
-
-	SELECT @nidout AS nidout, @bidout AS bidout, @locidout AS locidout, @cbgidout AS cbgidout, @vidout AS vidout;
-
     END;
 END;
+
+SELECT @nidout AS 'nidout', @bidout AS 'bidout', @locidout AS 'locidout', @cbgidout AS 'cbgidout', @vidout AS 'vidout';
